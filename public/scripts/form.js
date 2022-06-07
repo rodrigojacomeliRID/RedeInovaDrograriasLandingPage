8178b11ac6094822c2fd5c3b0e41e117c557f802
rodrigojacomeliRID <rodrigo.jacomeli.redeinova@gmail.com>
2022-06-07 16:31:54 -0300

    Add code JS from loading form RD

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,4 +1,10 @@
-new RDStationForms(
-  'formulario-teste-jacomeli-80d478c8189c4f25cc06',
-  'UA-126383576-2'
-).createForm();
+window.onload = function () {
+  async function CreateFormRD() {
+    new window.RDStationForms(
+      'formulario-teste-jacomeli-80d478c8189c4f25cc06',
+      'UA-126383576-2'
+    ).createForm();
+  }
+
+  CreateFormRD();
+};
